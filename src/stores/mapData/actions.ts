8e3b9d4f3aacd7b5c4fd64f8f57b11c756ab180c
Caique Gaspar <caiqsgaspar@hotmail.com>
2022-05-26 10:34:39 -0300

    teste na requisição axios

--- src/stores/mapData/actions.ts
+++ src/stores/mapData/actions.ts
@@ -1,8 +1,14 @@
 import { api } from "configs/axios";
+import axios from "axios";
 
 const actions = {
 	async getUserData(): Promise<void> {
-		const response = await api({ method: "GET" });
+		// const response = await api({ method: "GET" });
+		const response = await axios({
+			method: "get",
+			url: "https://ancestry.caiquegaspar.workers.dev/",
+			headers: { "Data-Name": "user" },
+		});
 		const result = response.data.resposta;
 
 		// @ts-ignore
